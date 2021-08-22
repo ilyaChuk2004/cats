229e41d3e13ada821c781134ec3ab9c366bff6c2
ilyaChuk2004 <iliyachuk3004@gmail.com>
2021-08-22 15:25:29 +0300

    для десктопов новый переход между страницами

--- js/routes.js
+++ js/routes.js
@@ -5,6 +5,12 @@ import SettingsPage from '../pages/settings.f7.html';
 import water from '../pages/water.f7.html';
 import waterSet from '../pages/waterSettings.f7.html';
 import avaChange from '../comps/avaChange.f7.html';
+import Framework7 from 'framework7/bundle';
+
+let transition = 'f7-parallax'
+if (Framework7.device == 'desktop' || Framework7.device == 'ipad' || window.innerWidth >=750) {
+  transition = 'f7-circle'
+}
 
 var routes = [
 
@@ -12,30 +18,48 @@ var routes = [
   
   {
     path: '/',
-    component: HomePage,
+    component: HomePage,  
+    options: {
+      transition: transition,
+    },
   },
   {
     path: '/catalog/',
-    component: CatalogPage,
+    component: CatalogPage, 
+    options: {
+      transition: transition,
+    },
   },
   {
     path: '/settings/',
-    component: SettingsPage,
+    component: SettingsPage,  
+    options: {
+      transition: transition,
+    },
   },
 
 
   {
     path: '/water/',
-    component: water,
+    component: water, 
+    options: {
+      transition: transition,
+    },
   },
   {
     path: '/waterSet/',
-    component: waterSet,
+    component: waterSet,  
+    options: {
+      transition: transition,
+    },
   },
 
 
   {
-    path: '/popup-content/',
+    path: '/popup-content/',  
+    options: {
+      transition: transition,
+    },
     popup: {
       component: avaChange,
       closeOnEscape: true,
